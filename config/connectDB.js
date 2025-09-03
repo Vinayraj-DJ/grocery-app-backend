cb044288081a90527750ac985655efd80652d4ea
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2025-09-03 13:36:55 +0530

    Updated backend code

--- config/connectDB.js
+++ config/connectDB.js
@@ -28,6 +28,6 @@ export const connectDB = async () => {
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ Error connecting to MongoDB:", error.message);
-    process.exit(1); // Exit the process with failure
+ 
   }
 };
